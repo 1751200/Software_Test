6fb8ed4007bcd063c9c008ba124b79aa17dbd410
VodkaSoul <vodkasoul@icloud.com>
2020-07-01 02:03:12 +0800

    Update doc.py

--- q9/doc.py
+++ q9/doc.py
@@ -26,33 +26,33 @@ $$
 + 0->d->a->b1->b2->e->13->14->15->16->15->14->13->g->a->21'''
 
 
-code = r'''void ModuleX (int x, int y, int Wid, char *Str) 
-{
-    unsigned Zcode, Bcode; 
-    int i, j, k, Rec, Color; 
-    long Len; 
-    char Buf[72]; 
-    while (*Str) 
-    {
-        if ((*Str & 0x80) && (*(Str+1) & 0x80))
-        {
-            Zcode = (*Str - 0xa1) & 0x07f;
-            Bcode = (*(Str + 1) - 0xa1) & 0x07f;
-            Rec = Zcode*94 + Bcode;
-            Len = Rec*72L;
-            fseek(fp, Len, SEEK_SET);
-            fread (Buf, 1, 72, fp);
-            for (i = 0; i < 24; i++) 
-                for (j = 0; j < 3; j++) 
-                    for (k = 0; k < 8; k++) 
-                        if (Buf[i*3 + j] >> (7 - k) & 1) 
-                        { 
-                            Color = y + j*8 + k - 46; 
-                            PutPoint(x + i, y + j*8 + k, Color); 
-                        }
-            x = x + 24 + Wid;
-            Str += 2;
-        }
-    } 
-    return;
-}'''
+code = r'''0 void ModuleX (int x, int y, int Wid, char *Str) 
+  {
+1    unsigned Zcode, Bcode; 
+2    int i, j, k, Rec, Color; 
+3    long Len; 
+4    char Buf[72]; 
+5    while (*Str) 
+     {
+6        if ((*Str & 0x80) && (*(Str+1) & 0x80))
+         {
+7             Zcode = (*Str - 0xa1) & 0x07f;
+8             Bcode = (*(Str + 1) - 0xa1) & 0x07f;
+9             Rec = Zcode*94 + Bcode;
+10            Len = Rec*72L;
+11            fseek(fp, Len, SEEK_SET);
+12            fread (Buf, 1, 72, fp);
+13            for (i = 0; i < 24; i++) 
+14                for (j = 0; j < 3; j++) 
+15                    for (k = 0; k < 8; k++) 
+16                        if (Buf[i*3 + j] >> (7 - k) & 1) 
+                          { 
+17                            Color = y + j*8 + k - 46; 
+18                            PutPoint(x + i, y + j*8 + k, Color); 
+18-1                      }
+19            x = x + 24 + Wid;
+20            Str += 2;
+6-1        }
+5-1    } 
+21    return;
+  }'''
